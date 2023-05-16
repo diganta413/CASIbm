7a9df779af96b598a6277ce0ffa21cfeca40cace
Diganta <git.innovizia@gmail.com>
2023-05-16 23:59:55 +0530

    Supplier Admin Page Done

--- IBMApr2023Batch_CASProj/Controllers/SupplierController.cs
+++ IBMApr2023Batch_CASProj/Controllers/SupplierController.cs
@@ -3,15 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using IBMApr2023Batch_CASProj.Models;
+using IBMApr2023Batch_CASProj.Models.SecurityModel;
 
-namespace IBM_CAS.Controllers
+namespace IBMApr2023Batch_CASProj.Controllers
 {
+    [Authorize]
     public class SupplierController : Controller
     {
+        IBMApr2023Batch_CASProj.Models.SecurityModel.CASIbmEntities _db = new IBMApr2023Batch_CASProj.Models.SecurityModel.CASIbmEntities();
         // GET: Supplier
         public ActionResult Index()
         {
-            return View();
+            return View(_db.Orders.ToList());
+        }
+        public ActionResult OrderDetails(int? id)
+        {
+            Order order = _db.Orders.Find(id);
+            return View(order);
         }
     }
 }
